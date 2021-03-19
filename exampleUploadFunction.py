2b148fdce0921efeacd6698267c5839a6f9cb700
Raned Chuphueak <6210612864@student.tu.ac.th>
2021-03-19 23:28:13 +0700

    update

--- exampleUploadFunction.py
+++ exampleUploadFunction.py
@@ -1,13 +1,13 @@
 import os
 
-from testManage import FileManager
+from myDataLib import FileManager
 
 
 def main():
-    #Choose your Folder and sent it's file to class
+    # Choose your Folder and sent it's file to class
     data = [file for file in os.listdir("Glaucoma")]
     f = FileManager("Glaucoma")
-    #Upload your file from list:data to your drive
+    # Upload your file from list:data to your drive
     while len(data) != 0:
         file = data.pop()
         f.upload_file(f"Glaucoma/{file}", file)

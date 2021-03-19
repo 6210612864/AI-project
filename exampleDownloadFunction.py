d2c06c0e0c96243f6f2ebeff88a627c0cb3b2e2a
Raned Chuphueak <6210612864@student.tu.ac.th>
2021-03-19 23:25:36 +0700

    upload example how to use Download Upload function

---
+++ exampleDownloadFunction.py
@@ -0,0 +1,15 @@
+from myDataLib import FileManager
+
+
+def main():
+    #Select folder in your drive
+    func = FileManager("Normal")
+    #get list of dictionary in your folder
+    data = func.clist_lfif()
+    #download file by sent your list and index 
+    func.download_file(data[0])
+    print("finish")
+    #this will download n0001.png
+
+
+main()
---
+++ exampleUploadFunction.py
@@ -0,0 +1,17 @@
+import os
+
+from testManage import FileManager
+
+
+def main():
+    #Choose your Folder and sent it's file to class
+    data = [file for file in os.listdir("Glaucoma")]
+    f = FileManager("Glaucoma")
+    #Upload your file from list:data to your drive
+    while len(data) != 0:
+        file = data.pop()
+        f.upload_file(f"Glaucoma/{file}", file)
+        print(f"Uploaded {file}")
+
+
+main()
